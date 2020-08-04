11d721c5c3121bf6789ca86f42b67b1e86f05746
Eduardo Telmo Da Silva Barros <therazortec@gmail.com>
2020-08-04 00:59:37 -0300

    Rotas e Pagina de erro adicionadas

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TeacherForm() {
+
+    return (
+      <>
+      <h1>teste</h1>
+      </>
+    );
+
+}
+
+
+export default TeacherForm;
---
+++ web/src/routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Landing/index';
+import TeacherList from './pages/Teacherlist/index';
+import TeacherForm from './pages/TeacherForm/index';
+import Erro404 from './pages/Erros/Erro404';
+
+
+
+function Routes() {
+
+    return (
+        <BrowserRouter>
+
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/study" exact component={TeacherList} />
+                <Route path="/give-classes" exact component={TeacherForm} />
+                <Route component={Erro404} />
+            </Switch>
+
+        </BrowserRouter>
+    );
+
+}
+
+
+
+export default Routes;
